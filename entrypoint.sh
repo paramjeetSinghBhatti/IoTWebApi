31d4a84c86e5c83fc9c743cecb47a2c85e0b5ceb
paramjeetSinghBhatti <paramjeet.sbhatti@gmail.com>
2024-11-25 13:24:10 +0100

    fix file name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Replace placeholders in appsettings.production.json with environment variables
-sed -i "s|\${DB_SERVER}|${DB_SERVER}|g" /app/appsettings.production.json
-sed -i "s|\${DB_NAME}|${DB_NAME}|g" /app/appsettings.production.json
-sed -i "s|\${DB_USER}|${DB_USER}|g" /app/appsettings.production.json
-sed -i "s|\${DB_PASSWORD}|${DB_PASSWORD}|g" /app/appsettings.production.json
+sed -i "s|\${DB_SERVER}|${DB_SERVER}|g" /app/appsettings.Production.json
+sed -i "s|\${DB_NAME}|${DB_NAME}|g" /app/appsettings.Production.json
+sed -i "s|\${DB_USER}|${DB_USER}|g" /app/appsettings.Production.json
+sed -i "s|\${DB_PASSWORD}|${DB_PASSWORD}|g" /app/appsettings.Production.json
 
 # Start the application
 exec dotnet IoTWebApi.dll
